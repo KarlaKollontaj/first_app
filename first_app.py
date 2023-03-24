05836fec1f3205366b0532522894651d947858b8
KarlaKollontaj <carlavaleriabarnab@gmail.com>
2023-03-24 12:42:28 +0100

    requirements

---
+++ first_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+x1=st.slider('peso in kg', 0, 120, 50)
+x2=st.slider('altezza in m', 0.0, 2.0, 1.5)
+
+
+def bmi(a:float,b:float):
+    bmi=a/(b*b)
+    return bmi
+
+bmi= bmi(x1,x2)
+#st.write(bmi)
+
+if 16< bmi <19:
+    st.write('il tuo bmi è', round(bmi,2), 'sei sottopeso')
+elif  19< bmi <25:
+    st.write('il tuo bmi è', round(bmi,2), 'sei normopeso')
+else:#elif 25< bmi <30:
+    st.write('il tuo bmi è', round(bmi,2), 'sei sovrappeso')
+
